640e33c5c51add295736a5c92bcc676a3661fdc9
jumbu moses <jambcareer@gmail.com>
2022-10-26 21:07:28 +0300

    head first

---
+++ sacco_shield/calculation_table.php
@@ -0,0 +1,26 @@
+<?php 
+extract($_POST);
+
+$monthly = ($amount + ($amount * ($interest/100))) / $months;
+$penalty = $monthly * ($penalty/100);
+
+?>
+<hr><!--  Author Name: Mayuri K. 
+ for any PHP, Codeignitor, Laravel OR Python work contact me at mayuri.infospace@gmail.com  
+ Visit website : www.mayurik.com -->  
+<table width="100%">
+	<tr>
+		<th class="text-center" width="33.33%">Total Payable Amount</th>
+		<th class="text-center" width="33.33%">Monthly Payable Amount</th>
+		<th class="text-center" width="33.33%">Penalty Amount</th>
+	</tr>
+	<tr>
+		<td class="text-center"><small><?php echo number_format($monthly * $months,2) ?></small></td>
+		<td class="text-center"><small><?php echo number_format($monthly,2) ?></small></td>
+		<td class="text-center"><small><?php echo number_format($penalty,2) ?></small></td>
+	</tr>
+</table>
+<hr>
+<!--  Author Name: Mayuri K. 
+ for any PHP, Codeignitor, Laravel OR Python work contact me at mayuri.infospace@gmail.com  
+ Visit website : www.mayurik.com -->  
---
+++ sacco_shield/topbar.php
@@ -0,0 +1,31 @@
+<style>
+	.logo {
+    margin: auto;
+    font-size: 35px;
+   /* background: white;
+    padding: 7px 11px;
+    border-radius: 50% 50%;
+    color: #000000b3;*/
+}
+</style>
+<!--  Author Name: Mayuri K. 
+ for any PHP, Codeignitor, Laravel OR Python work contact me at mayuri.infospace@gmail.com  
+ Visit website : www.mayurik.com -->  
+<div id="page"></div>
+<div id="loading"></div>
+<nav class="navbar navbar-light fixed-top" style="padding:0;">
+  <div class="container-fluid mt-2 mb-2">
+  	<div class="col-lg-12 d-flex align-items-center justify-content-between">
+  		<div class="col-md-2 float-left" style="display: flex;">
+  			<div class="logo">
+  				<img src="assets/img/logo.png" width="320px">
+  			</div>
+  		</div>
+     
+	  	<div class="col-md-6 text-right text-dark">
+	  		<strong><a href="https://www.mayurik.com/#download_section" target="a_blank" class="text-dark">Contact Author : Mayuri K.</a></strong>
+	    </div>
+    </div>
+  </div>
+  
+</nav>
